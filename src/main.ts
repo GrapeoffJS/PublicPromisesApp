66cfca26f2443a9dd8c8b2194326990fe54d425b
GrapeoffJS <idmitriy.git@gmail.com>
2022-01-09 18:32:53 +0300

    new cors rules

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,21 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors();
-    app.use(helmet());
+    app.use(
+        helmet({
+            contentSecurityPolicy: {
+                directives: {
+                    defaultSrc: [`'self'`],
+                    styleSrc: [`'self'`, `'unsafe-inline'`, 'cdn.jsdelivr.net'],
+                    scriptSrc: [
+                        `'self'`,
+                        'unsafe-inline',
+                        `cdnjs.cloudflare.com`
+                    ]
+                }
+            }
+        })
+    );
 
     app.useGlobalPipes(
         new ValidationPipe({
