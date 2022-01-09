3cf10f345199c022154594514148af6ca70973bd
GrapeoffJS <idmitriy.git@gmail.com>
2022-01-09 21:02:46 +0300

    ...

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors();
-    app.use(helmet());
+    app.use(
+        helmet({
+            contentSecurityPolicy: false
+        })
+    );
 
     app.useGlobalPipes(
         new ValidationPipe({
